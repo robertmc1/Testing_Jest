7de2b619abf9ce0aca4ac8fc88e141d11559827f
Robert Machi <robert.mc@hotmail.es>
2019-02-27 06:30:03 +0100

    First component test

--- server/app.js
+++ server/app.js
@@ -2,12 +2,32 @@ require('./config');
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const CityModel = require('./models/City');
+
 const server = express();
 
 server.use(bodyParser.json());
 
 server.get('/', (req, res) => {
     res.send({message: 'Funciona!'});
+});
+
+server.post("./city", async (req, res) => {
+    // res.status( 201).send()
+    // new Cities({...req.body})
+    //     .save()
+    //     .then(city => {
+    //         res.status(201).send(city);
+    //     })
+    //     .catch(err => {
+    //        res.status(400).send(err);
+    // })
+    try {
+        const city = await new Cities({...req.body}).save();
+        res.status(201).send(city);
+    } catch (err) {
+        res.status(400).send(err);
+    }
 });
 
 module.exports = server;
--- server/test/routes/City/City.test.js
+++ server/test/routes/City/City.test.js
@@ -1,37 +1,105 @@
 const request = require('supertest');
+const {ObjectID} = require ('mongodb');
 const server = require('../../../app');
+const Cities = require('../../../models/City');
 const {citiesData, populateCities} = require ('./City.seed');
 
-// populateCities();
+const baseURL = '/city';
+
+beforeEach( populateCities);
 
 describe('/city', () => {
-
-    const baseURL = '/city';
-
     describe('POST /', () => {
-        it('should return 403 without token', () => {
+        it('should return 201 body has right format', (done) => {
+            const newCity = {
+                ...citiesData[0],
+                _id: new ObjectID().toHexString(),
+                name: 'Zaragoza'
+            };
 
-        });
-
-        it('should return 401 without data', () => {
-
-        });
-
-        it('should return 201 w/data', (done) => {
            request(server)
                .post(baseURL)
                .set('Authorization', 'token')
                .send({
-                   //... body que le enviamos
+                   ...newCity
                })
                .expect(201)
-               .expect(res => {
-                   //... expect sobre el response.body (res.data en axios)
-               })
-               .end((err, res) => {
-                   //... preguntamos a la base de datos si todo ha ido bien
-                   done(err)
+               .expect(
+                   res => {
+                       expect(res.body).toMachObject(newCity);
+                   })
+               .end(async (err, res) => {
+                   if(err)
+                       return done(err);
+
+                   const city = await Cities.findById(res.body._id);
+                   expect({
+                       ...city.toObject(),
+                       _id: new ObjectID(city._id).toHexString()
+                   }).toMachObject(newCity);
+
+                   done();
                })
         });
+
+        it('should return 400 without address', (done) => {
+            const newCity = {
+                _id: new ObjectID().toHexString(),
+                name: 'Zaragoza'
+            };
+
+            request(server)
+                .post(baseURL)
+                .set('Authorization', 'token')
+                .send({...newCity})
+                .expect(400)
+                .end(async (err, res) => {
+                    if(err)
+                        return done(err);
+
+                    const city = await Cities.findById(newCity._id);
+                    expect(city).toBe(null);
+
+                    done();
+                })
+        });
+
+        it('should return 400 without name', (done) => {
+            const newCity = {
+                _id: new ObjectID().toHexString(),
+                address:'C/ Ppim Pam Pum'
+            };
+
+            request(server)
+                .post(baseURL)
+                .set('Authorization', 'token')
+                .send({...newCity})
+                .expect(400)
+                .end(async (err, res) => {
+                    if(err)
+                        return done(err);
+
+                    const city = await Cities.findById(newCity._id);
+                    expect(city).toBe(null);
+
+                    done();
+                })
+        });
+
+        it('should return 400 without data', (done) => {
+            request(server)
+                .post(baseURL)
+                .set('Authorization', 'token')
+                .expect(400)
+                .end(done)
+        });
+
+        it.skip('should return 400 without token', (done) => {
+            request(server)
+                .post(baseURL)
+                .expect(401)
+                .end(done)
+        });
+
     });
 });
